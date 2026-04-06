a9b96658d6dc93990d9f7516636ee13d362f3845
agle <a.michael1@uq.edu.au>
2026-04-06 23:59:02 +1000

    push

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,5 @@
 open CalendarLib
+open Containers
 
 let parsedate d = CalendarLib.Printer.Date.from_fstring "%F" d
 let today = Calendar.now ()
@@ -19,8 +20,7 @@ type meta = {
 
 module M = Map.Make (String)
 
-let url = ref "http://localhost:8000/"
-let site_title = ref "site title"
+type site_cfg = { url : string; title : string }
 
 let body m =
   m.content () |> function
@@ -34,32 +34,33 @@ let date m =
   | Some x -> x
   | None -> ""
 
-let to_item m =
+let to_item cfg m =
   let pubdate : Ptime.t option =
     let d = m.date |> Option.map Date.to_unixfloat in
     Option.bind d Ptime.of_float_s
   in
   let link =
     Uri.canonicalize
-    @@ Uri.of_string (Filename.concat !url (m.filename ^ ".html"))
+    @@ Uri.of_string (Filename.concat cfg.url (m.filename ^ ".html"))
   in
   let guid = Rss.Guid_permalink link in
   Rss.item ~title:m.name ~data:body ?pubdate ~link ~guid ()
 
-let gen_rss (m : meta) (ms : meta list) =
+let gen_rss cfg (m : meta) (ms : meta list) =
   let default_date x =
     match x with Some d -> d | None -> parsedate "1970-01-01"
   in
   let link =
     Uri.canonicalize @@ Uri.of_string
-    @@ Filename.concat !url (m.filename ^ ".xml")
+    @@ Filename.concat cfg.url (m.filename ^ ".xml")
   in
   let met =
     ms
-    |> List.filter (fun m -> not (m.template = `Rss))
+    |> List.filter (fun m -> not (Equal.poly m.template `Rss))
     |> List.sort (fun mi mj ->
         Calendar.Date.compare (default_date mi.date) (default_date mj.date))
-    |> List.rev |> List.map to_item
+    |> List.rev
+    |> List.map (to_item cfg)
   in
   let now = today |> CalendarLib.Printer.Calendar.sprint "%c" in
 
@@ -69,7 +70,7 @@ let gen_rss (m : meta) (ms : meta list) =
   Rss.print_channel f channel;
   Buffer.to_bytes b |> Bytes.to_string
 
-let inject_template (temp : string) (m : meta) =
+let inject_template cfg (temp : string) (m : meta) =
   let tag_re =
     Str.regexp
       {|%%\(CONTENTBODY\|CONTENTDATE\|CONTENTTITLE\|URL\|SITETITLE\|TOC\|GENTIME\|SITEURL\)%%|}
@@ -77,19 +78,19 @@ let inject_template (temp : string) (m : meta) =
   Str.global_substitute tag_re
     (fun x ->
       match Str.matched_string x with
-      | "%%SITEURL%%" -> !url
-      | "%%URL%%" -> Filename.concat !url (m.filename ^ ".html")
+      | "%%SITEURL%%" -> cfg.url
+      | "%%URL%%" -> Filename.concat cfg.url (m.filename ^ ".html")
       | "%%TOC%%" -> ( match m.toc with Some x -> x | None -> "")
       | "%%GENTIME%%" -> today |> CalendarLib.Printer.Calendar.sprint "%c"
       | "%%CONTENTBODY%%" -> body m
       | "%%CONTENTDATE%%" -> date m
       | "%%CONTENTTITLE%%" -> m.name
-      | "%%SITETITLE%%" -> !site_title
+      | "%%SITETITLE%%" -> cfg.title
       | x -> failwith x)
     temp
 
-let make_list_item item_template (ms : meta list) =
-  List.map (inject_template item_template) ms |> String.concat "\n"
+let make_list_item cfg item_template (ms : meta list) =
+  List.map (inject_template cfg item_template) ms |> String.concat "\n"
 
 let read_file ic =
   let res = ref "" in
@@ -101,7 +102,7 @@ let read_file ic =
   in
   read ic
 
-let sub_list (temp : string) (ms : meta list M.t) : string =
+let sub_list cfg (temp : string) (ms : meta list M.t) : string =
   let delim = Str.regexp {|%%BEGINLIST%%\([^%]+\)%%\|%%ENDLIST%%|} in
   let bgdeli = Str.regexp {|%%BEGINLIST%%\([^%]+\)%%|} in
   let n = Str.full_split delim temp in
@@ -118,13 +119,13 @@ let sub_list (temp : string) (ms : meta list M.t) : string =
         in
         let met =
           met
-          |> List.filter (fun m -> not (m.template = `Rss))
+          |> List.filter (fun m -> not (Equal.poly m.template `Rss))
           |> List.sort (fun mi mj ->
               Calendar.Date.compare (default_date mi.date)
                 (default_date mj.date))
           |> List.rev
         in
-        unpack (acc ^ make_list_item t met) rest
+        unpack (acc ^ make_list_item cfg t met) rest
     | Str.Text b :: rest -> unpack (acc ^ b) rest
     | [] -> acc
     | Str.Delim "%%ENDLIST%%" :: rest -> unpack acc rest
@@ -132,7 +133,9 @@ let sub_list (temp : string) (ms : meta list M.t) : string =
   in
   unpack "" n
 
-let extension s : string = String.split_on_char '.' s |> List.rev |> List.hd
+let extension s : string =
+  Filename.extension s |> fun e ->
+  Option.get_or ~default:e @@ String.chop_prefix ~pre:"." e
 
 let drop_exn s : string =
   String.split_on_char '.' s |> List.rev |> List.tl |> List.rev
@@ -142,9 +145,11 @@ let parse_m fs f =
   let ls = ref [] in
   let n = ref "" in
   while
-    (n := input_line f;
-     !n)
-    <> "---"
+    not
+    @@ String.equal
+         (n := input_line f;
+          !n)
+         "---"
   do
     ls := !n :: !ls
   done;
@@ -197,7 +202,7 @@ let build fs =
   let f = open_in fs in
   let n = ref (input_line f) in
   let filename = fs in
-  if !n = "---" then parse_m fs f
+  if String.equal !n "---" then parse_m fs f
   else (
     (* Meta that encodes direct copy *)
     close_in f;
@@ -229,23 +234,6 @@ let templates_dir = ref "templates"
 let source_dir = ref "src"
 let build_dir = ref "build"
 
-let speclist =
-  [
-    ( "-i",
-      Arg.Set_string source_dir,
-      "set input directory, default: " ^ !source_dir );
-    ( "-o",
-      Arg.Set_string build_dir,
-      "set output directory, default: " ^ !build_dir );
-    ( "--templates",
-      Arg.Set_string templates_dir,
-      "set templates directory, default: " ^ !templates_dir );
-    ("--url", Arg.Set_string url, "set site public url, default: " ^ !url);
-    ( "--title",
-      Arg.Set_string site_title,
-      "set site name title, default: " ^ !site_title );
-  ]
-
 let load_templs dir =
   list_directory dir
   |> List.map (fun f ->
@@ -276,7 +264,15 @@ let rec list_directory_rec_build dir : (string * meta list) list =
     (dir, files) :: dirs)
   else []
 
-let process_dir temps indir outdir =
+let rmdirrec odir =
+  let files =
+    CCIO.File.walk_l odir
+    |> List.filter_map (function `File, e -> Some e | _ -> None)
+  in
+  files |> List.iter CCIO.File.remove_noerr
+
+let process_dir cfg temps indir outdir =
+  rmdirrec outdir;
   let cd = Sys.getcwd () in
   let temps = load_templs temps in
   M.iter (fun i _ -> print_endline ("template " ^ i)) temps;
@@ -299,12 +295,12 @@ let process_dir temps indir outdir =
           | `Default -> (m.content (), m.filename)
           | `Fname f ->
               let t = M.find f temps in
-              let s = sub_list t bm in
-              let s = inject_template s m in
+              let s = sub_list cfg t bm in
+              let s = inject_template cfg s m in
               (`Text s, m.filename ^ ".html")
           | `Rss ->
               let u = Filename.dirname m.filename in
-              let s = gen_rss m (M.find u bm) in
+              let s = gen_rss cfg m (M.find u bm) in
               (`Text s, m.filename ^ ".xml")
         in
         (m, fname, templ))
@@ -328,10 +324,106 @@ let process_dir temps indir outdir =
         output_string os cont;
         close_out os)
   in
-  List.iter writeout built
+  List.iter writeout built;
+  Sys.chdir cd
 
 let usage_msg = "sb [ optional args ]"
 
+let serve ~config ~timeout (dir : string) addr port j : _ result =
+  let module S = Tiny_httpd in
+  let server = S.create ~max_connections:j ~addr ~port ~timeout () in
+  let after_init () =
+    Printf.printf "serve directory %s on http://%(%s%):%d\n%!" dir
+      (if S.is_ipv6 server then "[%s]" else "%s")
+      addr (S.port server)
+  in
+  Tiny_httpd.Dir.add_dir_path ~config ~dir ~prefix:"" server;
+  S.run ~after_init server
+
+let poll_dir dir callback =
+  let selectors =
+    Inotify.[ S_Modify; S_Delete; S_Move; S_Create; S_Close_write ]
+  in
+  let p = Inotify.create () in
+  CCIO.File.walk_l dir
+  |> List.iter (function
+    | `File, e -> ignore @@ Inotify.add_watch p e selectors
+    | `Dir, d -> ignore @@ Inotify.add_watch p d selectors);
+  let rec update () : unit =
+    let ev = Inotify.read p in
+    let do_update = true in
+    ev
+    |> List.iter (fun ((watch, ek, c, path) : Inotify.event) ->
+        List.map
+          (function
+            | Inotify.Create ->
+                Option.iter
+                  (fun e -> ignore @@ Inotify.add_watch p e selectors)
+                  path
+            | Inotify.Delete -> Inotify.rm_watch p watch
+            | _ -> ())
+          ek
+        |> ignore;
+        ());
+    if do_update then callback ();
+    update ()
+  in
+  update ()
+
+let httpd site_cfg port () =
+  let odir = Filename.temp_dir "preview" "d" in
+  if not @@ CCIO.File.is_directory odir then Sys.mkdir odir 0o755;
+
+  let config = Tiny_httpd.Dir.config ~dir_behavior:Index () in
+  let addr = "127.0.0.1" in
+  let port = port |> Int.of_string |> Option.get_exn_or "port not an int" in
+  let url = Printf.sprintf "http://%s:%d" addr port in
+  let callback =
+   fun () -> process_dir { site_cfg with url } !templates_dir !source_dir odir
+  in
+  callback ();
+  let timeout = 30. in
+  let serve_thread =
+    Thread.create
+      (fun () ->
+        serve ~config ~timeout odir addr port 2 |> function
+        | Ok _ -> ()
+        | Error e -> raise e)
+      ()
+  in
+  let poll_thread =
+    Thread.create (fun () -> poll_dir !source_dir callback) ()
+  in
+  Thread.join serve_thread;
+  Thread.join poll_thread;
+  rmdirrec odir;
+  ()
+
 let () =
+  let url = ref "http://localhost:8000/" in
+  let site_title = ref "site title" in
+  let preview = ref None in
+  let speclist =
+    [
+      ( "-i",
+        Arg.Set_string source_dir,
+        "set input directory, default: " ^ !source_dir );
+      ( "-o",
+        Arg.Set_string build_dir,
+        "set output directory, default: " ^ !build_dir );
+      ( "--templates",
+        Arg.Set_string templates_dir,
+        "set templates directory, default: " ^ !templates_dir );
+      ("--url", Arg.Set_string url, "set site public url, default: " ^ !url);
+      ( "--preview",
+        Arg.String (fun (u : string) -> preview := Some u),
+        "preview site port, default: disabled" );
+      ( "--title",
+        Arg.Set_string site_title,
+        "set site name title, default: " ^ !site_title );
+    ]
+  in
+  let cfg : site_cfg = { url = !url; title = !site_title } in
   Arg.parse speclist (fun f -> ()) usage_msg;
-  process_dir !templates_dir !source_dir !build_dir
+  !preview |> Option.iter (fun p -> httpd cfg p ());
+  process_dir cfg !templates_dir !source_dir !build_dir
---
+++ src/about.md
@@ -0,0 +1,5 @@
+---
+title: hello
+template: post.html
+date: 2026-03-15
+---
