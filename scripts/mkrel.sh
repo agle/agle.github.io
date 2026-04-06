84720e6c2c3896d60e32ea064b2071ec05756bd5
agle <a.michael1@uq.edu.au>
2026-04-07 01:20:36 +1000

    artifact

---
+++ scripts/mkrel.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -xe
+
+wget https://github.com/megastep/makeself/releases/download/release-2.7.1/makeself-2.7.1.run 
+bash makeself-2.7.1.run
+
+mkdir -p binout/lib
+
+LIB=$(ldd _build/default/bin/main.exe | grep libonig | cut -f3 -d' ')
+cp $LIB binout/lib
+opam exec -- dune build --profile=release
+cp _build/default/bin/main.exe binout/
+
+echo "#!/usr/bin/env bash" > binout/start.sh
+echo "LD_LIBRARY_PATH=lib ./main.exe" >> binout/start.sh
+
+bash makeself-2.7.1/makeself.sh binout sg "site generator" ./start.sh
